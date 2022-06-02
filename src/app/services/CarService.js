586ad48edfc7d74f1d9238261ab31005c3174106
Paulo Rayann <paulorpsena@gmail.com>
2022-06-02 12:57:28 -0300

    feat: add car CRUD service

--- src/app/services/CarService.js
+++ src/app/services/CarService.js
@@ -7,6 +7,27 @@ class CarService {
         return result
     }
 
+    async list(payload) {
+        const result = await CarRepository.list(payload)
+        return result
+    }
+
+    async getById(payload) {
+        const result =  await CarRepository.getById(payload)
+        return result
+    }
+
+    async updateCar(id, body) {
+        const result = await CarRepository.updateCar(id, body)
+        return result
+    }
+
+    async deleteCar(payload) {
+        const result = await CarRepository.deleteCar(payload)
+        return result
+    }
+
+
 }
 
 module.exports = new CarService()
