4213c315317c31639d9aa9c5a34b5a3c5c2fc66a
Paulo Rayann <paulorpsena@gmail.com>
2022-06-02 12:56:55 -0300

    feat: car CRUD controller

--- src/app/controllers/CarController.js
+++ src/app/controllers/CarController.js
@@ -9,6 +9,43 @@ class CarController {
             return res.status(400).json(error)
         }
     }
+
+    async list(req, res) {
+        const payload= req.query
+        try {
+            const result = await CarService.list(payload)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async getById(req, res) {
+        try {
+            const result = await CarService.getById(req.params.id)
+            return res.status(200).json(result)
+        } catch(error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const result =  await CarService.updateCar(req.params.id, req.body)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            const result = await CarService.deleteCar(req.params.id)
+            return res.status(200).json('success')
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
 }
 
 module.exports = new CarController()
