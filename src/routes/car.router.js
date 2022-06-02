eecee39ea12d30eeac9606f1508ae076ac309c76
Paulo Rayann <paulorpsena@gmail.com>
2022-06-02 12:58:07 -0300

    feat: add car CRUD routes

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -3,6 +3,10 @@ const CarController = require('../app/controllers/CarController')
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/', CarController.create)
+    routes.get('/', CarController.list)
+    routes.get('/:id', CarController.list)
+    routes.put('/:id', CarController.update)
+    routes.delete('/:id', CarController.delete)
 
 
     server.use(prefix, routes)
