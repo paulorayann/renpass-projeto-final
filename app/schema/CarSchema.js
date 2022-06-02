81714cc7c2bec7b94b6b1e5f6226fba5d536c1fd
Paulo Rayann <paulorpsena@gmail.com>
2022-06-01 22:45:19 -0300

    feat: add Car schema

---
+++ app/schema/CarSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const CarSchema = new mongoose.Schema ({
+
+    model: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    accessories: [{
+        type: String
+    }],
+    passengersQtd: {
+        type: number
+    }
+})
+
+const Car = mongoose.model('Car', CarSchema)
+module.exports = Car
