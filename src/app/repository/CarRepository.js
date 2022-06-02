725a05d538546a6820e5b833ff668427f2c65f3c
Paulo Rayann <paulorpsena@gmail.com>
2022-06-02 12:57:18 -0300

    feat: add car CRUD repository

--- src/app/repository/CarRepository.js
+++ src/app/repository/CarRepository.js
@@ -4,6 +4,22 @@ class CarRepository {
     async create(payload) {
         return CarSchema.create(payload)
     }
+
+    async list(payload) {
+        return CarSchema.find(payload)
+    }
+
+    async getById(payload) {
+        return CarSchema.findById(payload)
+    }
+
+    async updateCar(id, body) {
+        return CarSchema.findByIdAndUpdate(id, body)
+    }
+
+    async deleteCar(payload) {
+        return CarSchema.findByIdAndDelete(payload)
+    }
 }
 
 module.exports = new CarRepository()
