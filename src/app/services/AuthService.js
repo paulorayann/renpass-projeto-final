826b8d396d4931ea919d7cd42e596d5313530892
Paulo Rayann <paulorpsena@gmail.com>
2022-06-04 17:11:41 -0300

    feat: add authentication service

---
+++ src/app/services/AuthService.js
@@ -0,0 +1,22 @@
+const PersonRepository = require('../repository/PersonRepository');
+const bcrypt = require('bcryptjs');
+const Person = require('../schema/PersonSchema');
+
+class AuthService {
+    async auth(email, password) {
+        const person = await PersonRepository.auth(email);
+
+        if (!person) {
+            throw new Error('Person not found');
+        }
+
+        const validPassword = await bcrypt.compare(password, person.password);
+        if (!validPassword) {
+            throw new Error('Invalid password');
+        }
+
+        return { person };
+    }
+}
+
+module.exports = new AuthService();
