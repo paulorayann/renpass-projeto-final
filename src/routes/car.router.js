753d2cc53f246e10b0fd9525bebcf2f4928b0cf8
Paulo Rayann <paulorpsena@gmail.com>
2022-06-05 11:00:46 -0300

    fix: car get by id not working

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -3,7 +3,7 @@ const CarController = require('../app/controllers/CarController');
 module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/', CarController.create);
     routes.get('/', CarController.list);
-    routes.get('/:id', CarController.list);
+    routes.get('/:id', CarController.getById);
     routes.put('/:id', CarController.update);
     routes.patch('/:id', CarController.update);
     routes.delete('/:id', CarController.delete);
