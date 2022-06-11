6800278e67b8ffc18b41c57e632bdd4a9d8fed19
Paulo Rayann <paulorpsena@gmail.com>
2022-06-11 17:47:22 -0300

    feat: add cnpj validation

--- src/app/validations/rental/validateRental.js
+++ src/app/validations/rental/validateRental.js
@@ -1,10 +1,18 @@
 const Joi = require('joi').extend(require('@joi/date'));
+const { cnpjValid } = require('../../utils/regex');
+const cnpjValidation = require('../../utils/cnpjValidation');
 module.exports = async (req, res, next) => {
     try {
         const rental = Joi.object({
             name: Joi.string().trim().required(),
 
-            cnpj: Joi.string().trim().required(),
+            cnpj: Joi.string()
+                .trim()
+                .regex(cnpjValid)
+                .message(
+                    'The CNPJ field has an invalid format, please try XX.XXX.XXX/XXXX-XX and use numbers only'
+                )
+                .required(),
 
             activities: Joi.string().trim().required(),
 
@@ -34,6 +42,12 @@ module.exports = async (req, res, next) => {
                     }
                 ]
             };
+        if (!cnpjValidation(req.body.cnpj)) {
+            throw {
+                message: 'Bad Request',
+                details: 'Invalid CNPJ'
+            };
+        }
 
         return next();
     } catch (error) {
