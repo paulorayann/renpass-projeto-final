e5d272b122f942799950f50351344e964165be06
Paulo Rayann <paulorpsena@gmail.com>
2022-06-14 20:27:55 -0300

    style: add port const as process.env port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
+const dotenv = require('dotenv').config;
 
-const port = 3000;
+const port = process.env.DB_PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
