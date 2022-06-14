852c52fda68898c10dc378a75431d0480f099a4e
Paulo Rayann <paulorpsena@gmail.com>
2022-06-14 16:22:20 -0300

    fix: update rental throw error

--- src/app/services/RentalService.js
+++ src/app/services/RentalService.js
@@ -32,7 +32,7 @@ class RentalService {
     }
     async updateRental(id, body) {
         const result = await RentalRepository.updateRental(id, body);
-        if (!result) throw new NotFound(id);
+        if (!result) throw new NotFound(id, body);
         return result;
     }
     async deleteRental(payload) {
