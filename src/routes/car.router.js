ceda0be9a08d41a97e88f4f28c2feca590b7f007
Paulo Rayann <paulorpsena@gmail.com>
2022-06-15 03:26:00 -0300

    feat: add cars requiring token bearer

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,8 +1,10 @@
 const CarController = require('../app/controllers/CarController');
 const validateCar = require('../app/validations/car/validateCar');
 const validateCarUpdate = require('../app/validations/car/validateCarUpdate');
+const authToken = require('../app/middlewares/authToken');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
+    routes.use(authToken);
     routes.post('/', validateCar, CarController.create);
     routes.get('/', CarController.list);
     routes.get('/:id', CarController.getById);
