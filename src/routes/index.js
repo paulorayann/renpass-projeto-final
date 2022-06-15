d2efcafbb9f7891e163a76ddf9a930eff4b37e76
Paulo Rayann <paulorpsena@gmail.com>
2022-06-15 00:37:44 -0300

    feat: add swagger route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const car = require('./car.router');
 const person = require('./person.router');
 const auth = require('./auth.router');
 const rental = require('./rental.router');
+const swagger = require('./swagger.router');
 
 module.exports = (server) => {
     server.use((req, res, next) => {
@@ -10,6 +11,7 @@ module.exports = (server) => {
         car(server, new Router());
         auth(server, new Router());
         rental(server, new Router());
+        swagger(server, new Router());
         next();
     });
 };
