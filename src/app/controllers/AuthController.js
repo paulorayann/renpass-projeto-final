9eeed90465c2ab97340e1b7068ca9183f1c284d0
Paulo Rayann <paulorpsena@gmail.com>
2022-06-15 03:52:26 -0300

    refactor: change auth res status

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -5,7 +5,7 @@ class AuthController {
         try {
             const { email, password } = req.body;
             const result = await AuthService.auth(email, password);
-            return res.status(201).json(result);
+            return res.status(200).json(result);
         } catch (error) {
             return res.status(404).json({
                 error: error.message
