0813d32e791301b78b7755907575c7316567f584
Paulo Rayann <paulorpsena@gmail.com>
2022-06-18 16:27:46 -0300

    refactor: move cpf validation to service

--- src/app/services/PersonService.js
+++ src/app/services/PersonService.js
@@ -1,16 +1,24 @@
 const PersonRepository = require('../repository/PersonRepository');
 const NotFound = require('../errors/NotFound');
+const cpfValidation = require('../utils/cpfValidation');
 
 class PersonService {
     async create(payload) {
+        if (!cpfValidation(payload.cpf)) {
+            throw {
+                message: 'Please enter a valid CPF'
+            };
+        }
         const result = await PersonRepository.create(payload);
         if (!result) throw new Error(payload);
+
         return result;
     }
 
     async list(payload) {
         const result = await PersonRepository.list(payload);
         if (!result) throw new Error(payload);
+
         return result;
     }
 
@@ -20,8 +28,15 @@ class PersonService {
         return result;
     }
 
-    async updatePerson(id, body) {
-        const result = await PersonRepository.updatePerson(id, body);
+    async updatePerson(id, payload) {
+        if (payload.cpf) {
+            if (!cpfValidation(payload.cpf)) {
+                throw {
+                    message: 'Please enter a valid CPF'
+                };
+            }
+        }
+        const result = await PersonRepository.updatePerson(id, payload);
         if (!result) throw new NotFound(id);
         return result;
     }
