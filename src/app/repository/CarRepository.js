cd3cc66cb2e437eb7374fd86e90286241ecd6584
Paulo Rayann <paulorpsena@gmail.com>
2022-06-18 21:45:10 -0300

    refactor: add extend of Repository Pattern to car repository

--- src/app/repository/CarRepository.js
+++ src/app/repository/CarRepository.js
@@ -1,57 +1,23 @@
 const CarSchema = require('../schema/CarSchema');
+const Repository = require('./RepositoryPattern');
 
-class CarRepository {
-    async create(payload) {
-        return CarSchema.create(payload);
+class CarRepository extends Repository {
+    constructor() {
+        super(CarSchema);
     }
-
     async list(query) {
-        const { limit, offset } = query;
         const customLabels = {
-            totalDocs: 'total',
-            docs: 'Car',
-            page: 'offset',
-            nextPage: false,
-            prevPage: false,
-            totalPages: 'offsets',
-            pagingCounter: false,
-            meta: false,
-            hasPrevPage: false,
-            hasNextPage: false
+            docs: 'vehicles'
         };
-
         const options = {
-            limit: parseInt(limit, 10) || 10,
-            offset: parseInt(offset, 0) || 1,
             customLabels: customLabels
         };
-        return CarSchema.paginate(query, options, {});
-    }
-
-    async getById(payload) {
-        return CarSchema.findById(payload);
-    }
-
-    async updateCar(id, body) {
-        return CarSchema.findByIdAndUpdate(id, body);
-    }
-
-    async deleteCar(payload) {
-        return CarSchema.findByIdAndDelete(payload);
+        return CarSchema.paginate(query, options);
     }
 
     async updateCarAccessory(id, accessoryId, updatedAccessory) {
         const result = await CarSchema.findOneAndUpdate(
             { _id: id, 'accessories._id': accessoryId },
-            // {
-            //     $pull: {
-            //         accessories: {
-            //             description: {
-            //                 $eq: ''
-            //             }
-            //         }
-            //     }
-            // },
             {
                 $set: {
                     'accessories.$.description': updatedAccessory.description
