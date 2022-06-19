5146f9c28bbc69715e638c67e5883555bd9c5faa
Paulo Rayann <paulorpsena@gmail.com>
2022-06-18 21:45:27 -0300

    refactor: add extend of Repository Pattern to person repository

--- src/app/repository/PersonRepository.js
+++ src/app/repository/PersonRepository.js
@@ -1,46 +1,18 @@
 const PersonSchema = require('../schema/PersonSchema');
+const Repository = require('./RepositoryPattern');
 
-class PersonRepository {
-    async auth(email) {
-        return PersonSchema.findOne({ email }).select('+password');
+class PersonRepository extends Repository {
+    constructor() {
+        super(PersonSchema);
     }
-
-    async create(payload) {
-        return PersonSchema.create(payload);
-    }
-
     async list(query) {
-        const { limit, offset } = query;
         const customLabels = {
-            totalDocs: 'total',
-            docs: 'Person',
-            page: 'offset',
-            nextPage: false,
-            prevPage: false,
-            totalPages: 'offsets',
-            pagingCounter: false,
-            meta: false,
-            hasPrevPage: false,
-            hasNextPage: false
+            docs: 'person'
         };
         const options = {
-            limit: parseInt(limit, 10) || 10,
-            offset: parseInt(offset, 0) || 1,
             customLabels: customLabels
         };
-        return PersonSchema.paginate(query, options, {});
-    }
-
-    async getById(payload) {
-        return PersonSchema.findById(payload);
-    }
-
-    async updatePerson(id, payload) {
-        return PersonSchema.findByIdAndUpdate(id, payload);
-    }
-
-    async deletePerson(payload) {
-        return PersonSchema.findByIdAndDelete(payload);
+        return PersonSchema.paginate(query, options);
     }
 }
 
