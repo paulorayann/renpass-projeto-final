20a645424e57033b58d89ff03e7c5c40ac7a6ab0
Paulo Rayann <paulorpsena@gmail.com>
2022-06-18 21:45:41 -0300

    refactor: add extend of Repository Pattern to rental repository

--- src/app/repository/RentalRepository.js
+++ src/app/repository/RentalRepository.js
@@ -1,39 +1,18 @@
 const RentalSchema = require('../schema/RentalSchema');
-const axios = require('axios').default;
+const Repository = require('./RepositoryPattern');
 
-class RentalRepository {
-    async create(payload) {
-        return RentalSchema.create(payload);
+class RentalRepository extends Repository {
+    constructor() {
+        super(RentalSchema);
     }
     async list(query) {
-        const { limit, offset } = query;
         const customLabels = {
-            totalDocs: 'total',
-            docs: 'Rentals',
-            page: 'offset',
-            nextPage: false,
-            prevPage: false,
-            totalPages: 'offsets',
-            pagingCounter: false,
-            meta: false,
-            hasPrevPage: false,
-            hasNextPage: false
+            docs: 'rentals'
         };
         const options = {
-            limit: parseInt(limit, 10) || 10,
-            offset: parseInt(offset, 0) || 1,
             customLabels: customLabels
         };
-        return RentalSchema.paginate(query, options, {});
-    }
-    async getById(payload) {
-        return RentalSchema.findById(payload);
-    }
-    async updateRental(id, body) {
-        return RentalSchema.findByIdAndUpdate(id, body);
-    }
-    async deleteRental(payload) {
-        return RentalSchema.findByIdAndDelete(payload);
+        return RentalSchema.paginate(query, options);
     }
 }
 
