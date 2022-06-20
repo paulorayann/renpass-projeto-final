d48dc73f9ad0d186357e97b395acb5b9d7c678cf
Paulo Rayann <paulorpsena@gmail.com>
2022-06-19 21:21:54 -0300

    feat: add reserve and fleet routers in index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const car = require('./car.router');
 const person = require('./person.router');
 const auth = require('./auth.router');
 const rental = require('./rental.router');
+const reserve = require('./reserve.router');
+const fleet = require('./fleet.router');
 const swagger = require('./swagger.router');
 
 module.exports = (server) => {
@@ -11,6 +13,8 @@ module.exports = (server) => {
         car(server, new Router());
         auth(server, new Router());
         rental(server, new Router());
+        reserve(server, new Router());
+        fleet(server, new Router());
         swagger(server, new Router());
         next();
     });
