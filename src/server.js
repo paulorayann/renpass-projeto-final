ba68359aa66b20c64c76f4d33489b1169c61d8ca
Paulo Rayann <paulorpsena@gmail.com>
2022-06-20 17:27:33 -0300

    fix: change process env port for Heroku deploy

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.DB_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
