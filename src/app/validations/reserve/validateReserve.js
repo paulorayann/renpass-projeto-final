71f2657dec9cbdc81787996cf4c1af972c8abbcf
Paulo Rayann <paulorpsena@gmail.com>
2022-06-20 22:10:29 -0300

    feat: add Reserve create and update validations

---
+++ src/app/validations/reserve/validateReserve.js
@@ -0,0 +1,37 @@
+const Joi = require('joi').extend(require('@joi/date'));
+const { objectId } = require('../../utils/regex');
+
+module.exports = async (req, res, next) => {
+    try {
+        const reserve = Joi.object({
+            id_user: Joi.string()
+                .regex(objectId)
+                .message('Please enter a valid Id format (ObjectId)')
+                .required(),
+            data_start: Joi.date().format('DD/MM/YYYY').required(),
+            data_end: Joi.date().format('DD/MM/YYYY').required(),
+            id_car: Joi.string()
+                .regex(objectId)
+                .message('Please enter a valid Id format (ObjectId)')
+                .required(),
+            id_rental: Joi.string()
+                .regex(objectId)
+                .message('Please enter a valid Id format (ObjectId)'),
+            final_value: Joi.number().min(1).required()
+        });
+
+        const { error } = await reserve.validate(req.body, {
+            abortEarly: false
+        });
+
+        if (error) throw error;
+        return next();
+    } catch (error) {
+        return res.status(400).json(
+            error.details.map((detail) => ({
+                description: detail.message,
+                name: detail.path.join('.')
+            }))
+        );
+    }
+};
--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -1,10 +1,20 @@
 const ReserveController = require('../app/controllers/ReserveController');
+const validateReserve = require('../app/validations/reserve/validateReserve');
+const validateReserveUpdate = require('../app/validations/reserve/validateReserveUpdate');
 
 module.exports = (server, routes, prefix = '/api/v1/rental') => {
-    routes.post('/:rentalId/reserve', ReserveController.create);
+    routes.post(
+        '/:rentalId/reserve',
+        validateReserve,
+        ReserveController.create
+    );
     routes.get('/:rentalId/reserve', ReserveController.list);
     routes.get('/:rentalId/reserve/:id', ReserveController.getById);
-    routes.patch('/:rentalId/reserve/:id', ReserveController.update);
+    routes.patch(
+        '/:rentalId/reserve/:id',
+        validateReserveUpdate,
+        ReserveController.update
+    );
     routes.delete('/:rentalId/reserve/:id', ReserveController.delete);
 
     server.use(prefix, routes);
