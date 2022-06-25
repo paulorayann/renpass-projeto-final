6aa14847b5812fcc086d6805ec1a809da7c002e6
Paulo Rayann <paulorpsena@gmail.com>
2022-06-25 16:51:03 -0300

    refactor: remove custom CantDrive error

--- src/app/errors/CantDrive.js
+++
@@ -1,9 +0,0 @@
-class CantDrive extends Error {
-  constructor(user) {
-    super();
-    this.name = `Can't drive`;
-    this.message = `User '${user}' can't drive`;
-  }
-}
-
-module.exports = CantDrive;
